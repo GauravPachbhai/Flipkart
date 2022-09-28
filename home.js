fb3f22fab00707d24d022ff5df538c4c933083c0
GauravPachbhai <gauravpachbhai555@gmail.com>
2022-09-28 20:25:10 +0530

    code

---
+++ home.js
@@ -0,0 +1,10 @@
+function loadCopan(){
+    document.getElementById("copan").style.visibility = 'visibal';
+    document.getElementById('main').style.opacity = '0.7';
+
+}
+
+function closCopan(){
+    document.getElementById('copan').style.visibility = 'hidden';
+    document.getElementById('main').style.opacity = '1';
+}
