c2b3c4151f91c21711e426f916cc520cb6a32c01
GauravPachbhai <gauravpachbhai555@gmail.com>
2022-09-28 20:56:43 +0530

    code

--- home.js
+++ home.js
@@ -1,10 +1,11 @@
-function loadCopan(){
+function loadCopan() {
     document.getElementById("copan").style.visibility = 'visibal';
     document.getElementById('main').style.opacity = '0.7';
 
 }
 
-function closCopan(){
+function closCopan() {
     document.getElementById('copan').style.visibility = 'hidden';
     document.getElementById('main').style.opacity = '1';
 }
+
